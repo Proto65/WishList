311e3dd864afc63ed016f8446a66ad87d470071a
Tayyab Nabeel <tayyab2390@gmail.com>
2025-09-23 19:54:03 -0400

    Update

--- app/src/main/java/com/example/wishlist/WishListAdapter.kt
+++ app/src/main/java/com/example/wishlist/WishListAdapter.kt
@@ -1,9 +1,14 @@
 package com.example.wishlist
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 
 class WishListAdapter(private val items: MutableList<WishList>) : RecyclerView.Adapter<WishListAdapter.ViewHolder>() {
@@ -18,6 +23,15 @@ class WishListAdapter(private val items: MutableList<WishList>) : RecyclerView.A
         holder.itemNameText.text = item.item
         holder.itemPriceText.text = item.price
         holder.itemUrlText.text = item.URL
+
+        holder.itemView.setOnClickListener {
+            try {
+                val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(item.URL))
+                ContextCompat.startActivity(it.context, browserIntent, null)
+            } catch (e: ActivityNotFoundException) {
+                Toast.makeText(it.context, "Invalid URL for " + item.item, Toast.LENGTH_LONG).show()
+            }
+        }
 
         holder.itemView.setOnLongClickListener {
             items.removeAt(holder.adapterPosition)
